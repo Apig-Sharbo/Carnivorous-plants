276ea56f54f29c413a0fee6e4438e378009f7a00
Mgrdich <mgotm13@gmail.com>
2019-11-18 22:50:54 +0400

    code refactoring + bug fixes

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Just Relax and Explore</title>
+
+    <link rel="stylesheet" type="text/css" href="css/index.css"/>
+    <link rel="stylesheet" type="text/css" href="css/projectCSS.css"/>
+</head>
+
+<body>
+    <?php include "templates/menu.php"; ?>
+
+    <?php include "templates/Floter.php"; ?>
+
+    <?php include "templates/Content.php"; ?>
+</body>
+
+<script src="js/scripts.js"></script>
+
+</html>
